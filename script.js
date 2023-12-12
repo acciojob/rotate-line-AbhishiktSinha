2faebefafdf02b4269d9781e2b42944ce83f7c9a
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-12-12 16:56:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const line = document.querySelector(".line");
+let angle = 2;
+
+function rotate() {
+	line.style.transform = `rotate(${angle}deg)`;
+	angle += 2;
+}
+
+const id = setInterval(rotate, 20);
+setTimeout(()=>{
+	clearInterval(id)
+}, 4000);
